16e95e003902bf7edf1ddc131c487a11d4686714
John Greco <jonboy433@hotmail.com>
2018-03-20 12:19:49 -0400

    updated jquery to accomodate tablets v2

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import onMainNavButtonPressed from '../../assets/layout.js';
 
-declare var $:any;
+declare var $: any;
+declare var onMainNavButtonPressed: any;
+
+// Check user-agent for tablets; if so we need to change navbar behavior. We need to get rid of hover styles
+var isTablet: boolean;
 
 @Component({
   selector: 'app-navbar',
@@ -9,15 +14,24 @@ declare var $:any;
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  
-  constructor(private route: ActivatedRoute) { }
+
+  constructor(private route: ActivatedRoute) {
+    var ua = navigator.userAgent;
+    if (ua.includes('iPad')) {
+      isTablet = true;
+    }
+
+    else {
+      isTablet = false;
+    }
+
+  }
 
   ngOnInit() {
-    
     console.log('Inside NavBar OnInit');
-    
+
     // Fix the navbar to the top position when they scroll down
-    $(window).scroll(function(){
+    $(window).scroll(function () {
       if ($(this).scrollTop() > 136) {
         $("#main-nav-container").attr('class', 'has_content fixed');
       } else {
@@ -25,47 +39,67 @@ export class NavbarComponent implements OnInit {
       }
     });
 
-    var onMenuButtonPressed = function() {
-      $("#main-nav").toggleClass("visible"),
-      $(".main-nav-container-container").toggleClass("nav-open"),
-      $(document.body).toggleClass("nav-open") 
+    // Add function to menu item that removes the hovered class on click
+    $(document).ready(function(){
+      var topMenuItem = $('#main-nav > ul > li');
+      topMenuItem.on('touchstart', function(){
+        var btnClicked = $(this);
 
-  }
+        btnClicked.children('ul').css('visibility') == 'visible' ? btnClicked.children('ul').css('visibility','hidden') : btnClicked.children('ul').css('visibility','visible');
+        btnClicked.children('ul').css('opacity') == '0' ? btnClicked.children('ul').css('opacity','1') : btnClicked.children('ul').css('opacity','0');
+
+      }).children().not('a').on('touchstart', function(e){
+        e.stopPropagation();
+      })
+    });
+
+    // Add touchend event to menu item
+    $(document).ready(function () {
+      var menuItem = $('#main-nav ul li ul li');
+      menuItem.on('touchend', function () {
+        var btnClicked = $(this);
+
+        btnClicked.parent().css("visibility","hidden");
+        btnClicked.parent().css("opacity","0");
+ 
+      });
+    });
 
   }
 
   menuItemClicked() {
-
     let id = this.route.snapshot.fragment;
-    
-    if(id != undefined) {
+
+    if (id != undefined) {
       this.scrollToAnchor(id);
     } else {
       // no fragment - they clicked main item so scroll to top of page
-      $('html,body').animate({scrollTop: top},'fast'); 
+      $('html,body').animate({ scrollTop: top }, 'fast');
     }
 
-    //Close the nav bar when its clicked
-    var mq = window.matchMedia('(max-width: 770px)');
-    if(mq.matches){
-       $("#main-nav").toggleClass("visible"),
-      $(".main-nav-container-container").toggleClass("nav-open"),
-      $(document.body).toggleClass("nav-open")
-    } else {
+    // layout.js - close menu when menu item is clicked on small devices
+    // TODO: This should only happen on iPhones
+    $(onMainNavButtonPressed());
+  }
 
-    }
-    
+  menuItemClickedTablet() {
+    //
+
   }
 
   scrollToAnchor(id: string) {
     function scrollToAnchor(id) {
-      var tag = $("#"+id+"");
-      $('html,body').animate({scrollTop: tag.offset().top},'fast');
+      var tag = $("#" + id + "");
+      $('html,body').animate({ scrollTop: tag.offset().top }, 'fast');
     }
 
     // Call above jquery
     scrollToAnchor(id);
 
+  }
+
+  isTablet() {
+    return isTablet;
   }
 
 }
--- src/assets/layout.js
+++ src/assets/layout.js
@@ -1,1 +1,38 @@
-function createCountdown() { var e = $('[data-hide-countdown="false"]#wedding-countdown'), t = e.data("wedding-date"), i = moment(t).startOf("day").diff(moment().startOf("day"), "days"); if (0 === i) return e.html("Today!"); var n = Math.abs(i), s = 0 > i ? "ago" : "to go", o = 1 != n ? "days" : "day", r = n + " " + o + " " + s; i > 0 && (r += "!"), e.html(r) } var onMainNavButtonPressed = function () { $("#main-nav").toggleClass("visible"), $(".main-nav-container-container").toggleClass("nav-open"), $(document.body).toggleClass("nav-open") }, handleScroll = function () { var e = document.getElementById("main-nav-container"), t = document.getElementById("header") || document.getElementById("content"), i = e.clientHeight, n = t.getBoundingClientRect(); n.top <= i ? e.classList.add("fixed") : e.classList.remove("fixed") }, init = function () { document.domain = window.location.hostname.match(/\.theknot.com/) ? "theknot.com" : window.location.hostname, window.location.hash && $(window.location.hash).length > 0 && $("html, body").animate({ scrollTop: $(window.location.hash).eq(0).offset().top }, 0), createCountdown(), $("button.main-nav").on("click", onMainNavButtonPressed), $(".rsvp-link").on("click", segmentIoTrackRsvp), $(".visit-website").on("click", segmentIoWebsiteClickThrough), checkTheme(), checkEditButton(), checkLocationWrap(), resizeCoverPhoto(), disableTurbolinks() }; window.addEventListener("scroll", handleScroll), $(function () { init() }), $(document).on("page:load", function () { init() });
+function createCountdown() { 
+    var e = $('[data-hide-countdown="false"]#wedding-countdown'), 
+    t = e.data("wedding-date"), 
+    i = moment(t).startOf("day").diff(moment().startOf("day"), "days"); 
+    
+    if (0 === i) return e.html("Today!"); 
+    
+    var n = Math.abs(i), s = 0 > i ? "ago" : "to go", o = 1 != n ? "days" : "day", r = n + " " + o + " " + s; i > 0 && (r += "!"), 
+    e.html(r) } 
+    
+    var onMainNavButtonPressed = function () { 
+        $("#main-nav").toggleClass("visible"), 
+        $(".main-nav-container-container").toggleClass("nav-open"), 
+        $(document.body).toggleClass("nav-open") 
+    }, 
+        
+        handleScroll = function () { 
+            var e = document.getElementById("main-nav-container"), 
+                t = document.getElementById("header") || document.getElementById("content"), 
+                i = e.clientHeight,
+                n = t.getBoundingClientRect(); 
+            
+                n.top <= i ? e.classList.add("fixed") : e.classList.remove("fixed") 
+            }, 
+        
+        init = function () { 
+                document.domain = window.location.hostname.match(/\.theknot.com/) ? "theknot.com" : window.location.hostname, window.location.hash && $(window.location.hash).length > 0 && $("html, body").animate({ scrollTop: $(window.location.hash).eq(0).offset().top }, 0), 
+                createCountdown(), $("button.main-nav").on("click", onMainNavButtonPressed), $(".rsvp-link").on("click", segmentIoTrackRsvp), 
+                $(".visit-website").on("click", segmentIoWebsiteClickThrough), 
+                checkTheme(), 
+                checkEditButton(), 
+                checkLocationWrap(), 
+                resizeCoverPhoto(), 
+                disableTurbolinks() }; 
+        
+    window.addEventListener("scroll", handleScroll), $(function () { init() }), 
+                
+    $(document).on("page:load", function () { init() });
